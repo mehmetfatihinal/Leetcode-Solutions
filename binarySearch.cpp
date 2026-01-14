b34d5e8d53458e5c5b64775e006826e21533d53f
mehmetfatihinal <164665141+mehmetfatihinal@users.noreply.github.com>
2026-01-15 00:39:43 +0300

    Remove unused variable from binary search method
    Removed unused variable 'count' from search method.

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -3,12 +3,10 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int count = 0;
 
         for ( int i = 0; i<nums.size();i++){
             if (nums[i]==target){
                 return i;
-                count = 1;
             }
         }
         return -1;
